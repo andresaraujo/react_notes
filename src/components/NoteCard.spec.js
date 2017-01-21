f99ca518826d6b2e8e1aa1a4ca3b1552185630ab
Andres Araujo <a.araujo.azua@gmail.com>
2017-01-21 11:42:54 -0600

    (test): add missing enzyme dependency

--- src/components/NoteCard.spec.js
+++ src/components/NoteCard.spec.js
@@ -1,7 +1,7 @@
 /* eslint-env mocha */
 import React from 'react';
+import { assert } from 'chai';
 import {shallow} from 'enzyme';
-import {assert} from 'chai';
 import {IconButton, CardTitle} from 'material-ui';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
@@ -16,7 +16,6 @@ describe('<NoteCard />', () => {
         const wrapper = shallowWithContext(
             <NoteCard id="1" title="myTitle" content="myContent"/>
         );
-        assert.strictEqual(wrapper.is('NoteCard'), true);
         assert.strictEqual(wrapper.find(CardTitle).props().title, 'myTitle', 'CardTitle should contain title prop');
         assert.strictEqual(wrapper.find(CardTitle).props().subtitle, 'myContent', 'CardTitle should contain subtitle prop');
         assert.strictEqual(wrapper.find(IconButton).length, 1, 'should contain the delete icon button');
