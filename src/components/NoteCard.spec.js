cdf02e90fa721d26ca0ebc372a2e3142769e81a0
Andres Araujo <a.araujo.azua@gmail.com>
2017-01-21 11:32:44 -0600

    (test): Add NoteCard basic test

---
+++ src/components/NoteCard.spec.js
@@ -0,0 +1,24 @@
+/* eslint-env mocha */
+import React from 'react';
+import {shallow} from 'enzyme';
+import {assert} from 'chai';
+import {IconButton, CardTitle} from 'material-ui';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import NoteCard from './NoteCard';
+
+
+describe('<NoteCard />', () => {
+    const muiTheme = getMuiTheme();
+    const shallowWithContext = (node) => shallow(node, {context: {muiTheme}});
+
+    it('renders a note card', () => {
+        const wrapper = shallowWithContext(
+            <NoteCard id="1" title="myTitle" content="myContent"/>
+        );
+        assert.strictEqual(wrapper.is('NoteCard'), true);
+        assert.strictEqual(wrapper.find(CardTitle).props().title, 'myTitle', 'CardTitle should contain title prop');
+        assert.strictEqual(wrapper.find(CardTitle).props().subtitle, 'myContent', 'CardTitle should contain subtitle prop');
+        assert.strictEqual(wrapper.find(IconButton).length, 1, 'should contain the delete icon button');
+    });
+});
